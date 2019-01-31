b97a8965ff67218ac155ad2febac530c20b0900b
GilTorch <gilbert.torchon@esih.edu>
2019-01-31 09:51:51 -0500

    YEAAH

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,16 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    puts self.attraction.inspect
+    puts self.user.inspect
+    if self.attraction.tickets > self.user.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    end
+
+    if user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    end
+  end
 end
