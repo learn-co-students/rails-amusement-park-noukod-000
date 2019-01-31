8b31f0e8bf29ea873994e7c0c0426a31decc9746
GilTorch <gilbert.torchon@esih.edu>
2019-01-31 11:25:49 -0500

    old-code

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @user=User.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,24 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    #puts self.attraction.inspect
+    #puts self.user.inspect
+    if self.attraction.tickets > self.user.tickets
+      if user.height < attraction.min_height
+        return "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+      end
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    end
+    if user.height < attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{attraction.name}."
+    end
+
+    user.tickets-=attraction.tickets
+    user.nausea+=attraction.nausea_rating
+    user.happiness+=attraction.happiness_rating
+    user.save
+
+  end
 end
