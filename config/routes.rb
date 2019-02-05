8be4a0d8ce38731f6548678628bb5459a6193a6f
Samuel Cornet <samuel@noukod.com>
2019-02-05 18:56:12 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :users,only:[:index,:show,:create,:new,:update]
-  resources :attractions,only:[:index,:show,:new,:create,:edit,:update]
-  resources :rides,only:[:index,:show,:new,:update,:create]
-
-  get "/signin",to:"sessions#signin"
-  post "/signin",to:"sessions#create"
-  get "/logout",to:"sessions#destroy"
-
-  root "welcome#index"
+   resources :users
+   resources :sessions
+   resources :attractions
+   resources :rides, only: [:new]
+   root :to => 'welcome#greet'
+   get '/signin' => 'sessions#new'
+   get "/signout", to: "sessions#clear"
 
 end
