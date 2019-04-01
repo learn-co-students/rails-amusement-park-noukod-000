9a84ff745cb429db71bab4b8a69f15304184dff8
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-01 20:53:25 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,11 +3,12 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
-    conditions = ["height", "tickets", "nausea", "happiness"]
+    conditions = ["tickets", "height", "nausea", "happiness"]
     methods = Attraction.attribute_names
     # raise riding_errors("tickets")
-    raise conditions.map{|c|  return "user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first}" if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first})" }.first
-    # conditions.each{|c| return riding_errors(c) if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first} >= 0)" }
+    # raise conditions.map{|c|  return "user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first}" if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first})" }.first
+    errors = conditions.select{|c| riding_errors(c) if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first} < 0)" }
+    raise errors.inspect
     # (user.tickets - attraction.tickets >= 0) ? ((user.height - attraction.height >= 0) ? "" : "Sorry. You are not tall enough to ride the #{attraction.name}.") : "Sorry. You do not have enough tickets to ride the #{attraction.name}."
   end
 
@@ -16,7 +17,7 @@ class Ride < ActiveRecord::Base
   def riding_errors condition
     case condition
     when "height"
-      "Sorry. You are not tall enough to ride the #{attraction.name}. #{condition}"
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
     when "tickets"
       "Sorry. You do not have enough tickets to ride the #{attraction.name}."
     when "nausea"
