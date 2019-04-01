7b82e2732d8b4abcd1e2b6a597da8c7bb248da6d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-01 23:12:55 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,29 +3,30 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
-    conditions = ["tickets", "height", "nausea", "happiness"]
+    conditions = ["tickets", "height"]
     methods = Attraction.attribute_names
     # raise riding_errors("tickets")
     # raise conditions.map{|c|  return "user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first}" if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first})" }.first
-    errors = conditions.select{|c| riding_errors(c) if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first} < 0)" }
-    raise errors.inspect
+    "Sorry. #{conditions.select{|c| c if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first} < 0)" }.map { |e| user.update_userstate(attraction); riding_errors(e); }.join(" ")}"
+
+    # errors.map { |e| riding_errors(e)  }.join(" ")
+    # raise errors.inspect
     # (user.tickets - attraction.tickets >= 0) ? ((user.height - attraction.height >= 0) ? "" : "Sorry. You are not tall enough to ride the #{attraction.name}.") : "Sorry. You do not have enough tickets to ride the #{attraction.name}."
   end
 
   private
 
   def riding_errors condition
-    case condition
+    case (c = condition)
     when "height"
-      "Sorry. You are not tall enough to ride the #{attraction.name}."
+      "You are not tall enough to ride the #{attraction.name}."
     when "tickets"
-      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-    when "nausea"
-      ""
-    when "happiness"
-      ""
+      "You do not have enough tickets to ride the #{attraction.name}."
     end
-
   end
+
+  # def update_userstate
+  #   user.nausea=
+  # end
 end
 #
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
 
   has_secure_password
+
+  def update_userstate(attraction)
+    self.tickets = self.tickets - attraction.tickets
+    self.nausea = self.nausea + attraction.nausea_rating
+    self.happiness = self.happiness + attraction.happiness_rating
+  end
 end
