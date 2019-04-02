fa6ae297f0cd4681edc8c9d92e0f042a8a2526bf
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-02 16:18:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :is_loging?, :current_user
+  before_action :get_rooted
+
+  def is_loging?
+    !!current_user
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def get_rooted
+    redirect_to root_path unless is_loging?
+  end
 end
