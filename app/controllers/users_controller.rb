6fac9ba47e1dd90e5303c23d2b3b608e2d0adc26
Saint-Jean Germila <germila@noukod.com>
2019-04-08 18:21:19 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  # before_action :require_login
+  # skip_before_action :require_login,only:[:create]
+  def index
+  end
+
+  def new
+    @user=User.new
+  end
+
+
+  def update
+    @user=User.find(params[:id])
+    flash[:message]=@user.rides.last.take_ride
+    redirect_to user_path(@user)
+  end
+
+  def show
+    if session[:user_id].nil?
+      redirect_to "/"
+    end
+    @user=User.find(params[:id])
+  end
+
+  def create
+    @user=User.create(users_params)
+    if @user.valid?
+      session[:user_id]=@user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to users_path
+    end
+  end
+
+  def users_params
+     params.require(:user).permit(:name,:happiness,:tickets,:nausea,:password,:height, :admin)
+  end
+
+
+end
