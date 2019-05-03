4b6ebe5f2caf22abc929410f74b8e81c95f7c5e9
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-03 08:00:00 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,12 +5,24 @@ class Ride < ActiveRecord::Base
   def take_ride
     conditions = ["tickets", "height"]
     methods = Attraction.attribute_names
+    return "Sorry. #{conditions.select{|c|
+    c if eval "(user.#{c} - attraction.#{methods.select{|m|
+     m[/\A(\w)*(#{c})(\w)*\z/]}.first} < 0)" }.map { |e|
+     riding_errors(e) }.join(" ")}" if (user.tickets < attraction.tickets || user.height < attraction.min_height)
+
+    # dnt_have_enough_ticket = "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    # not_tall_enough = " You are not tall enough to ride the #{attraction.name}."
+    # return dnt_have_enough_ticket+not_tall_enough if !good_height && !good_tichket
+    # return dnt_have_enough_ticket unless good_tichket
+    # return "Sorry.#{not_tall_enough}" unless good_height
+
+
     self.user.update(tickets: user.tickets - attraction.tickets,
                 nausea: user.nausea + attraction.nausea_rating,
                 happiness: user.happiness + attraction.happiness_rating)
     # raise riding_errors("tickets")
     # raise conditions.map{|c|  return "user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first}" if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first})" }.first
-    "Sorry. #{conditions.select{|c| c if eval "(user.#{c} - attraction.#{methods.select{|m| m[/\A(\w)*(#{c})(\w)*\z/]}.first} < 0)" }.map { |e| riding_errors(e) }.join(" ")}"
+    "Thanks for riding the #{attraction.name}!"
   end
 
   private
