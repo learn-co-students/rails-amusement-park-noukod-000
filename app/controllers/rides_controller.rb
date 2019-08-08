4ad2191c778f88edc92693a42bea9566444b7749
Ricartho Franck <ricartho.franck@esih.edu>
2019-08-08 16:39:15 +0000

    save

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,20 +1,15 @@
 class RidesController < ApplicationController
   def create
-    ride = Ride.new(ride_params)
+    @ride = Ride.new(ride_params)
 
-    if ride.save
-        ride.take_ride
-        flash[:notice] = "#{ride.take_ride}"
-        redirect_to user_path(ride.user)
+    if @ride.take_ride
+        @ride.save
+        flash[:notice] = "#{@ride.take_ride}"
+        redirect_to user_path(@ride.user)
     else
-        flash[:notice] = "#{ride.take_ride}"
-        redirect_to user_path(ride.user)
+        flash[:notice] = "#{@ride.take_ride}"
+        redirect_to user_path(@ride.user)
     end
-    # if ride.save
-    #   redirect_to user_path(ride.user)
-    # else
-    #   render '/attractions/#{attraction_id}'
-    # end
   end
 
   private
