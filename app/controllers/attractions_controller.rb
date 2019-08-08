077e5e7ad58c46d3da2905375e1c8ddcb6c9a8b2
Ricartho Franck <ricartho.franck@esih.edu>
2019-08-08 17:30:42 +0000

    save

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,4 +8,23 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.find_by(:id => params[:id])
     @user = User.find_by(:id => session[:user_id])
   end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    attraction = Attraction.new(attraction_params)
+    if attraction.save
+      redirect_to attraction_path(attraction)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name,:nausea_rating,:happiness_rating,:min_height,:tickets)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, :only =>[:index,:new,:create,:show]
-  resources :attractions, :only =>[:index,:show]
+  resources :attractions, :only =>[:index,:show,:new,:create]
   resources :rides, :only => [:create]
   root 'application#home'
   get 'signin' => 'sessions#new'
