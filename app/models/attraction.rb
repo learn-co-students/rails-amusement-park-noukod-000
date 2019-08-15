b2e5782b915b5b2f37e8a4e0b0d6b69edb40fd6c
Billy Joseph <ingbillyjoseph@yahoo.fr>
2019-08-15 13:40:18 -0400

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,8 @@
 class Attraction < ApplicationRecord
-end
+    has_many :rides
+    has_many :users, through: :rides
+  
+    def users_count
+      self.users.count;
+    end
+  end
---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+son.partial! "users/user", user: @user
